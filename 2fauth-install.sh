97be1679b4b1c3072875c8a2d9a8b07781b70955
Felix Perez <felixalberto26@gmail.com>
2025-02-14 23:13:04 -0500

    Update 2fauth-install.sh

--- 2fauth-install.sh
+++ 2fauth-install.sh
@@ -53,16 +53,14 @@ cd "/opt/2fauth" || return
 cp .env.example .env
 IPADDRESS=$(hostname -I | awk '{print $1}')
 
-asterisk  = "*"
-
 sed -i -e "s|^APP_URL=.*|APP_URL=http://$IPADDRESS|" \
        -e "s|^DB_CONNECTION=$|DB_CONNECTION=mysql|" \
        -e "s|^DB_DATABASE=$|DB_DATABASE=$DB_NAME|" \
        -e "s|^DB_HOST=$|DB_HOST=127.0.0.1|" \
        -e "s|^DB_PORT=$|DB_PORT=3306|" \
        -e "s|^DB_USERNAME=$|DB_USERNAME=$DB_USER|" \
-       -e "s|^DB_PASSWORD=$|DB_PASSWORD=$DB_PASS|" 
-       -e "s|^TRUSTED_PROXIES=null$|TRUSTED_PROXIES=$asterisk|" .env
+       -e "s|^DB_PASSWORD=$|DB_PASSWORD=$DB_PASS|" \
+       -e "s|^TRUSTED_PROXIES=null$|TRUSTED_PROXIES=*|" .env
 
 export COMPOSER_ALLOW_SUPERUSER=1
 $STD composer update --no-plugins --no-scripts
